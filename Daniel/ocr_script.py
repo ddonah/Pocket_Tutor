ee89199329e47c785db3451298e9bb76ef99c7eb
ddonah <129772074+ddonah@users.noreply.github.com>
2024-04-22 21:33:59 -0700

    Add files via upload

--- Daniel/ocr_script.py
+++ Daniel/ocr_script.py
@@ -2,37 +2,70 @@ from detecto.core import Model
 from detecto.utils import read_image
 import numpy as np
 from PIL import Image
+from PIL import ImageOps
 import keras
+import pillow_heif
+import cv2
+import torch
+import cv2
 
-def image_processing():
-    '''
-    Still need to do this, but this will take the image file, use PIL to binzarize (make black and white a la a scanned document
-    and return the binarize image)
-    '''
-    pass
+def process_image(image_path):
+    if image_path.split('.')[-1] == 'HEIC':
+        heif_img = pillow_heif.read_heif(image_path)
+        img = Image.frombytes(
+            heif_img.mode,
+            heif_img.size,
+            heif_img.data,
+            'raw'
+        )
+        img = np.array(img)
+        if img.shape[2] == 4: 
+            img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
+    else:
+        img = cv2.imread(image_path)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, otsu_thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    adjusted_thresh_value = max(0, (_ - 0.11 * 255))
+
+    _, adjusted_thresh = cv2.threshold(blur, adjusted_thresh_value, 255, cv2.THRESH_BINARY_INV)
+    
+    final_image = cv2.bitwise_not(adjusted_thresh)
+    
+    pil_image = Image.fromarray(final_image)
+
+    return pil_image
 
 def line_detection(line_model, image, threshold=0.9):
 
-    _, boxes, scores = line_model.predict(image)
+    lines, boxes, scores = line_model.predict(image)
 
     high_confidence_indices = scores >= threshold
     filtered_boxes = boxes[high_confidence_indices]
 
-    return image, filtered_boxes
+    return image, lines, filtered_boxes
 
 def symbol_detection(symbol_model, image, box, threshold=0.9):
     #unfinished - fundamentally similar to line_detection
     cropped_image = image.crop((int(box[0]), int(box[1]), int(box[2]), int(box[3])))
 
-    cropped_img_array = np.array(cropped_image)
+    rgb_cropped_image = cropped_image.convert('RGB')
+    rgb_cropped_image_padded = ImageOps.expand(rgb_cropped_image, border=(0, 60, 0, 60), fill='white')
 
-    _, boxes, scores = symbol_model.predict(cropped_img_array)
+    #cropped_img_array = np.array(rgb_cropped_image_padded)
+
+    _, boxes, scores = symbol_model.predict(rgb_cropped_image_padded)
 
     high_confidence_indices = scores >= threshold
     filtered_boxes = boxes[high_confidence_indices]
+    filtered_scores = scores[high_confidence_indices]
+    characters = ['character'] * len(filtered_scores)
 
     #need to also do non-max suppresion
-    return filtered_boxes
+    return rgb_cropped_image_padded, filtered_boxes
 
 def symbol_recognition(recognition_model, image, box):
     cropped_image = image.crop((int(box[0]), int(box[1]), int(box[2]), int(box[3])))
@@ -50,9 +83,13 @@ def symbol_recognition(recognition_model, image, box):
     padded_image = Image.new("RGB", (max(width, height), max(width, height)), (255, 255, 255))
     padded_image.paste(cropped_image, (padding[0], padding[1]))
 
-    resized_image = Image.resize((45, 45), Image.LANCZOS)
+    resized_image = padded_image.resize((45, 45), Image.LANCZOS)
 
     resized_array = np.array(resized_image)
+
+    resized_array = resized_array.astype('float32') / 255.0
+
+    resized_array = np.expand_dims(resized_array, axis=0)
 
     proba = recognition_model.predict(resized_array)[0]
 
@@ -63,21 +100,67 @@ def symbol_recognition(recognition_model, image, box):
     pred = [x for x in top_classes if x in character_dct.keys()][0]
     return character_dct[pred]
 
-def scan_image(image_path):
-    image = image_processing(image_path)
+def check_multiplication(line_list):
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['+', '-', '*']
+    variables = ['x', 'y']
+    parenthensis = ['(', ')']
 
-    line_model = Model.load('complete_line_detection.pth', ['line'])
-    symbol_model = 'symbol_model'
+    positions = [index for index, value in enumerate(line_list) if value == 'x']
+    if positions:
+        for position in positions:
+            if (position > 0) and (position < len(line_list)-1):
+                if line_list[position - 1] in symbols or line_list[position + 1] in symbols:
+                    continue
+                elif line_list[position - 1] in numbers and line_list[position + 1] in numbers:
+                    line_list[position] = '*'
+                    continue
+    
+    return line_list
+
+def pipeline(image_path):
+    line_model = Model.load('line_detection.pth', ['line'])
+    character_detection_model = Model.load('character_detector.pth', ['character'])
     recognition_model = keras.models.load_model("mathsymbols.model")
 
-    line_boxes = line_detection(line_model, image)
-    image_result = []
+    image = process_image(image_path)
+    image_rgb = image.convert('RGB')
+    image_rgb, lines, line_boxes = line_detection(line_model, image_rgb)
 
+    _, sorted_indices = torch.sort(line_boxes[:, 1])
+    sorted_line_boxes = line_boxes[sorted_indices]
+    result_list = []
 
-    for box in line_boxes:
-        character_boxes = symbol_detection(symbol_model, image, box)
-        character_results = [symbol_recognition(recognition_model, image, x) for x in character_boxes]
-        line_string = character_results.join()
-        image_result.append(line_string)
+    for line_box in sorted_line_boxes:
+        line_list = []
 
-    return image_result
+        line_cropped_image, symbol_boxes = symbol_detection(character_detection_model, image, line_box)
+
+        _, sorted_indices = torch.sort(symbol_boxes[:, 0])
+        sorted_symbol_boxes = symbol_boxes[sorted_indices]
+
+        for symbol_box in sorted_symbol_boxes:
+            predicted_element = symbol_recognition(recognition_model, line_cropped_image, symbol_box)
+
+            line_list.append(predicted_element)
+        
+        line_list = check_multiplication(line_list)
+
+        result_list.append(line_list)
+    
+    result_list = [''.join(x) for x in result_list]
+
+    with open('unconfirmed_results.txt', 'w') as f:
+        for line in result_list:
+            f.write(f'{line}\n')
+
+    f.close()
+
+def main():
+    image_path = 'need-to-connect-this'
+
+    pipeline(image_path)
+
+if '__main__' in __name__:
+    main()
+
